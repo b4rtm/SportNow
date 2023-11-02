9f3a005d8abb473d791b6e1154795588d0193891
b4rtm <bartek.matusiak12@gmail.com>
2023-11-02 14:30:58 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8" />
+    <title>Tytuł strony...</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<nav class="navbar">
+    <a ><img src="logo.png" alt="Logo strony" class="logo"></a>
+    <ul class="navbar-menu">
+        <li class="navbar-item"><a href="#">Oferta</a></li>
+        <li class="navbar-item"><a href="#">O nas</a></li>
+        <li class="navbar-item"><a href="#">Kontakt</a></li>
+        <li class="navbar-item"><a href="#">Logowanie</a></li>
+        <li class="navbar-item"><a href="#">Rejestracja</a></li>
+    </ul>
+</nav>
+
+<div class="container">
+    <img class="main_photo" src="main_photo.jpg" alt="główne zdjecie">
+
+    <div class="main_header">Zarezerwuj swoje ulubione boisko już teraz!</div>
+    <a class="reserve_button">Rezerwuj</a>
+
+</div>
+
+</body>
+</html>
