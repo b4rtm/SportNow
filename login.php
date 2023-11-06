37ba9d625cc828bb92439d53c480eff9c051b0f1
b4rtm <bartek.matusiak12@gmail.com>
2023-11-06 20:21:46 +0100

    logout

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+global $conn;
+include 'head.php';
+include "navbar.php";
+
+require 'db/config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT user_id, password, name, surname FROM users WHERE email = :email";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $email);
+
+
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (password_verify($password, $row['password'])) {
+            session_start();
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['surname'] = $row['surname'];
+            header("Location: index.php");
+            exit();
+        } else {
+            // Invalid login
+            echo "Invalid email or password.";
+        }
+    }
+}
+?>
+
+
+<div class="register-page">
+    <div class="register-container">
+        <h1>Logowanie</h1>
+        <form class="register-fields" method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
+            <div class="register-field">
+                <label>email</label>
+                <input type="email" placeholder="email" name="email" required>
+            </div>
+            <div class="register-field">
+                <label>Hasło</label>
+                <input type="password" placeholder="Hasło" name="password" required>
+            </div>
+            <button type="submit" name="login">
+                Zaloguj
+            </button>
+            <a href="register.php">Nie mam jeszcze konta</a>
+        </form>
+    </div>
+</div>
--- navbar.php
+++ navbar.php
@@ -7,7 +7,15 @@
         <li class="navbar-item"><a href="reservation_offers.php">Oferta</a></li>
         <li class="navbar-item"><a href="#">O nas</a></li>
         <li class="navbar-item"><a href="#">Kontakt</a></li>
-        <li class="navbar-item"><a href="#">Logowanie</a></li>
+        <?php
+        session_start();
+        if (isset($_SESSION['name'])) {
+            ?>
+            <li class="navbar-item"><span>Zalogowany jako: <?= $_SESSION['name'] . ' ' . $_SESSION['surname']; ?></span></li>
+            <li class="navbar-item"><a href="logout.php">Wyloguj</a></li>
+        <?php } else { ?>
+        <li class="navbar-item"><a href="login.php">Logowanie</a></li>
         <li class="navbar-item"><a href="register.php">Rejestracja</a></li>
+        <?php } ?>
     </ul>
 </nav>
