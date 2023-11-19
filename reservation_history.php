2a9479a82cf8972568270b6346e6e23deb09f4ec
b4rtm <bartek.matusiak12@gmail.com>
2023-11-19 17:05:58 +0100

    implementation of reservation history page

--- reservation_history.php
+++ reservation_history.php
@@ -10,17 +10,25 @@ $reservations = getReservationsByUserId($_SESSION['user_id']);
 
 ?>
 
-<div id="your-reservations-page">
+<div id="your-reservations-page" xmlns="http://www.w3.org/1999/html">
     <h1>Witaj <?=  $_SESSION['name'] ?>!</h1>
-    <p>Oto twoje zamówienia</p>
-    <?php
-    // Wyświetl rezerwacje
-    foreach ($reservations as $reservation) {
-        echo '<div class="reservation">';
-        echo '<p>Data: ' . $reservation['date'] . '</p>';
-        echo '<p>Czas: ' . $reservation['start_time'] . '</p>';
-        // Dodaj inne informacje o rezerwacji
-        echo '</div>';
-    }
-    ?>
+    <h2 style="color: #aba9a9; margin-bottom: 45px;">Oto Twoje zamówienia:</h2>
+    <?php foreach ($reservations as $reservation): ?>
+        <h3> <?= $reservation['facility_name']?> </h3>
+        <div class="reservation">
+            <div class="info">
+                <p class="label">Data</p>
+                <p class="data"><?= $reservation['date']?></p>
+            </div>
+            <div class="info">
+                <p class="label">Czas trwania</p>
+                <p class="data"><?= substr($reservation['start_time'], 0, -3)?>-<?= substr($reservation['end_time'], 0, -3)?></p>
+            </div>
+            <div class="info">
+                <p class="label">Numer rezerwacji </p>
+                <p class="data"> <?= $reservation['reservation_id']?> </p>
+            </div>
+            <img src="<?= $reservation['image_path']?>" alt="faclity image">
+        </div>
+    <?php endforeach; ?>
 </div>
