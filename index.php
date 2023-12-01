21760f06813d743ac35653a183e8f98e2e53c561
b4rtm <bartek.matusiak12@gmail.com>
2023-12-01 10:46:29 +0100

    tiles on main page clickable

--- index.php
+++ index.php
@@ -10,22 +10,22 @@ include 'navbar.php';
 </div>
 <div style="background-color:#28303f;padding-bottom: 100px;"></div>
 <div class="tiles">
-    <div class="tile">
+    <a href="reservation_offers.php?sport=1" class="tile">
         <img src="images/main_page_images/football.png" alt="football image">
-        <a>Boiska do piłki nożnej</a>
-    </div>
-    <div class="tile">
+        <p>Boiska do piłki nożnej</p>
+    </a>
+    <a href="reservation_offers.php?sport=8" class="tile">
         <img src="images/main_page_images/running.png" alt="running image">
-        <a>Bieżnie</a>
-    </div>
-    <div class="tile">
+        <p>Bieżnie</p>
+    </a>
+    <a href="reservation_offers.php?sport=7" class="tile">
         <img src="images/main_page_images/tennis_man.png" alt="tennis image">
-        <a>Korty tenisowe</a>
-    </div>
-    <div class="tile">
-        <img src="images/main_page_images/basketball.png" alt="basketball image">
-        <a>Boiska do koszykówki</a>
-    </div>
+        <p>Korty tenisowe</p>
+    </a>
+    <a href="reservation_offers.php?sport=5" class="tile">
+            <img src="images/main_page_images/basketball.png" alt="basketball image">
+            <p>Boiska do koszykówki</p>
+    </a>
 </div>
 <div style="background-color:#28303f;padding-bottom: 100px;"></div>
 <script src="scripts/slider.js"></script>
